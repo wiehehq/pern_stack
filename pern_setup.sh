bd7a0ae24c0be57950719433a85e80d1bb7e4bf9
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-04-02 18:06:45 -0400

    Create pern_setup.sh

---
+++ pern_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Provide terminal user information about how to find instructions for setting up node.js
+echo "instructions on setting up node.js can be found at digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-16-04"
+
+
