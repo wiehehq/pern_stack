9f6183786f6b1aa5ee056fa15ab987e4a0267a25
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-04-09 18:32:08 -0400

    Create node_dependencies.sh

---
+++ building_blocks/node_dependencies.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+sudo -u dev-username curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+sudo -u dev-username command -v nvm
+sudo -u dev-username nvm --version
+sudo -u dev-username nvm install -y node
+sudo -u dev-username node --version
+sudo -u dev-username nvm install -y --lts
+sudo -u dev-username nvm alias default node
+sudo -u dev-username nvm ls
