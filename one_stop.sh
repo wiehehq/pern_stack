0aa2a23d9298b0d82792c15bf24bc0e1219f9285
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-04-09 19:42:55 -0400

    Update one_stop.sh

--- one_stop.sh
+++ one_stop.sh
@@ -1,13 +1,26 @@
 #!bin/bash
 
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username whoami]\n\n\n\n\n"
 sudo -u dev-username whoami
-sudo -u dev-username apt -y update && apt -y upgrade
-sudo -u dev-username apt install -y curl
+printf "\n\n\n\n\nCALLING ---> [$ sudo whoami]\n\n\n\n\n"
+sudo whoami
+sudo apt -y update
+sudo apt -y upgrade
+printf "\n\n\n\n\nCALLING ---> [$ sudo apt install -y curl]\n\n\n\n\n"
+sudo apt install -y curl
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash]\n\n\n\n\n"
 sudo -u dev-username curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username command -v nvm]\n\n\n\n\n"
 sudo -u dev-username command -v nvm
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm --version]\n\n\n\n\n"
 sudo -u dev-username nvm --version
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm install -y node]\n\n\n\n\n"
 sudo -u dev-username nvm install -y node
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username node --version]\n\n\n\n\n"
 sudo -u dev-username node --version
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm install -y --lts]\n\n\n\n\n"
 sudo -u dev-username nvm install -y --lts
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm alias default node]\n\n\n\n\n"
 sudo -u dev-username nvm alias default node
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm ls]\n\n\n\n\n"
 sudo -u dev-username nvm ls
