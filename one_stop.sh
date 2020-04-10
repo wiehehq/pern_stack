9608c7fe6e547292030564a7be22a4890c7471a8
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-04-09 20:49:49 -0400

    Create one_stop.sh

---
+++ one_stop.sh
@@ -0,0 +1,65 @@
+#!bin/bash
+
+
+# For easier copy/pasting
+printf "\n\n\n\n\nCALLING ---> [$ sudo]\n\n\n\n\n"
+sudo
+
+#printf "\n\n\n\n\nCALLING ---> [$ sudo apt install -y screen]\n\n\n\n\n"
+sudo apt install -y screen
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username whoami]\n\n\n\n\n"
+sudo -u dev-username whoami
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo whoami]\n\n\n\n\n"
+sudo whoami
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo apt -y update]\n\n\n\n\n"
+sudo apt -y update
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo apt -y upgrade]\n\n\n\n\n"
+sudo apt -y upgrade
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo apt install -y curl]\n\n\n\n\n"
+sudo apt install -y curl
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo apt install screen]\n\n\n\n\n"
+sudo apt install screen
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo screen -S part_one]\n\n\n\n\n"
+sudo screen -S part_one
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash]\n\n\n\n\n"
+sudo -u dev-username curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+#Now press Ctrl+A, D
+echo "screen part_one was been detached above, correct?"
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo screen -S part_two]\n\n\n\n\n"
+sudo screen -S part_two
+echo "screen part_two was been detached above, correct?"
+
+#printf "\n\n\n\n\nCALLING ---> [$ sudo sleep 5]\n\n\n\n\n"
+#sudo sleep 5
+
+exit
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username command -v nvm]\n\n\n\n\n"
+sudo -u dev-username command -v nvm
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm --version]\n\n\n\n\n"
+sudo -u dev-username nvm --version
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm install -y node]\n\n\n\n\n"
+sudo -u dev-username nvm install -y node
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username node --version]\n\n\n\n\n"
+sudo -u dev-username node --version
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm install -y --lts]\n\n\n\n\n"
+sudo -u dev-username nvm install -y --lts
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm alias default node]\n\n\n\n\n"
+sudo -u dev-username nvm alias default node
+
+printf "\n\n\n\n\nCALLING ---> [$ sudo -u dev-username nvm ls]\n\n\n\n\n"
+sudo -u dev-username nvm ls
